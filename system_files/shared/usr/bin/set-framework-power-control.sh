3c8bd0e15573948e53c91193682de815fcfe2227
Ray Gomez <ray.rgomez@gmail.com>
2024-01-19 13:36:11 -0700

    fix: be pedantic about starting/stopping services

--- system_files/shared/usr/bin/set-framework-power-control.sh
+++ system_files/shared/usr/bin/set-framework-power-control.sh
@@ -9,11 +9,12 @@ TLP="tlp"
 CPU_VENDOR=$(lshw -json -class CPU | jq -r ".[].vendor")
 
 disable_service() {
-  service=$1
+  service="$1"
 
   if systemctl is-enabled ${service}; then
     echo "Disabling ${service}"
-    systemctl disable --now "${service}"
+    systemctl stop ${service}
+    systemctl disable "${service}"
   else
     echo "${service} is not enabled"
   fi
@@ -26,14 +27,17 @@ enable_service() {
     echo "${service} is already enabled"
   else
     echo "Enabling ${service}"
-    systemctl enable --now "${service}"
+    systemctl enable "${service}"
+    systemctl start "${service}"
   fi
 }
 
 if [[ "${CPU_VENDOR^^}" =~ "AMD" ]]; then
-    disable_service ${TLP}
-    enable_service ${PPD}
+    disable_service "${TLP}"
+    enable_service "${PPD}"
 elif [[ "${CPU_VENDOR^^}" =~ "INTEL" ]]; then
-    disable_service ${PPD}
-    enable_service ${TLP}
+    disable_service "${PPD}"
+    enable_service "${TLP}"
 fi
+
+exit 0
